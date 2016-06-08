36c86b8152594aa0915048ae14eeb9f69969cd8f
MLateralus <michal.czerwien@gmail.com>
2016-06-08 17:54:30 +0200

    Choosem product now is filtered trough table to find all suppliers that match the product. Moreover onChange the result table is cleared for new resutls

--- js/asia.js
+++ js/asia.js
@@ -1,5 +1,9 @@
 // Asia handlers
 
+var buttonTable = document.getElementById("tablebutton");
+var buttonSupplier = document.getElementById("suppliersB");
+buttonTable.addEventListener("click", showTable);
+buttonSupplier.addEventListener("click", showSuppliers);
 
 function showTable(){
 	
@@ -14,12 +18,26 @@ function showTable(){
 	
 	$("#wholetable").find("td:nth-child(2)").each(function () {
 		var elem = this;
-		
 		var option = document.createElement("option")
 		option.innerHTML = elem.innerHTML;
 		document.getElementById("selectbasic").appendChild(option)
 	});
 })();
+
+function showSuppliers(){
+	
+	while (resultTable.childElementCount > 1) {
+		resultTable.removeChild(resultTable.lastChild);
+	}
+	
+	$.each($("#wholetable tbody").find("tr"), function() {
+		if($(this).text().toLowerCase().indexOf(getCurrentProduct().toLowerCase()) > -1){
+			var tmp = this.cloneNode(true);
+			document.getElementById("resultTable").appendChild(tmp);
+		}               
+    });
+	document.getElementById("resultSupplier").style.display = "block";
+}
 
 function getCurrentProduct(){
 	
@@ -27,7 +45,3 @@ function getCurrentProduct(){
 	console.log(suppDropdown.options[suppDropdown.selectedIndex].value);
 	return suppDropdown.options[suppDropdown.selectedIndex].value
 }
-
-var button = document.getElementById("tablebutton");
-button.addEventListener("click", showTable);
-
